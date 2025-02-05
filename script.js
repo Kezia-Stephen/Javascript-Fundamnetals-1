22e6145e80e305d22d23e39c5a0069d7344f8556
Kezia-Stephen <77871228+Kezia-Stephen@users.noreply.github.com>
2025-02-05 21:02:58 +0530

    Second commit

---
+++ script.js
@@ -0,0 +1,79 @@
+/*const country = "India";
+let population = 70;
+const isIsland = false;
+let language;
+console.log(typeof country, typeof population, typeof isIsland, typeof language);
+language = 'Tamil';
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+var description = country + ' is in Asia, and its ' + population + ' million people speak ' + language;
+console.log(description); */
+
+/*const firstName = "Kez";
+const job = "Mom";
+const birthYear = 1998;
+const year = 2025;
+
+const kezia = `I'm ${firstName}, a ${year - birthYear} years old ${job} ❤️`
+console.log(kezia);*/
+
+/*const age = 13;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log('You can start driving 💪🏼');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Please wait another ${yearsLeft} years to start driving`)
+}*/
+
+//type comversion
+/*const birthYear = 1998;
+const inputAge = Number(birthYear);
+console.log(inputAge + 18);
+
+console.log(String(inputAge), inputAge);*/
+
+//type coercion
+/*console.log("I'm " + 27 + " years old"); // JS converts the number also to string
+console.log('90' - '43' - 6);// JS converts all the strings to numbers
+console.log('90' / '10');*/
+
+//Falsy values: 0, '', undefined, null, NaN
+/*console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Kez'));
+console.log(Boolean({}));*/
+
+/*const age = 27;
+if (age === 18) {
+    console.log("You are an adult 🤓")
+}
+else {
+    console.log("Aww! You are just a child!")
+}*/
+
+//Boolean Logic: AND, OR and NOT
+/*const age = 18;
+if (age >= 20 && age < 30) {
+    console.log("AND operator returns true.")
+} else {
+    console.log("AND operator returns false.")
+}
+
+if (age >= 20 || age < 30) {
+    console.log("OR operator returns true.")
+} else {
+    console.log("OR operator returns false.")
+}
+
+if (!(age >= 20) && age < 30) {
+    console.log("NOT operator returns true.")
+} else {
+    console.log("NOT operator returns false.")
+}*/
+
+//SWITCH OPERATOR
